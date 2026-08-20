7c084b382546db7c692c8f0dccd14ea5323c85f0
intanhrdynt-medtech <intanhrdynt15@gmail.com>
2026-08-20 23:50:42 +0700

    Fix password_hash length for postgresql

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     full_name = db.Column(db.String(120), nullable=True)
     nim = db.Column(db.String(50), nullable=True)
     current_semester = db.Column(db.Integer, default=1)
